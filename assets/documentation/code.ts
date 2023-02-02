c2968c647a0d21c1aa43dc4cfababea469a3b2a9
Kifah Byish <kifah.byish@fmail.com>
2023-02-01 19:00:51 -0800

    test

---
+++ assets/documentation/code.ts
@@ -0,0 +1,58 @@
+    /*  
+    this method will accept array of words Ex\
+    dataArr=["abc", "def", ....];
+    spliChar= is the character that will be splilting the array based on
+    Ex: to get all words in "dataArr"
+    spliChar=" "; which will return array of words separated by space
+    Ex: spliChar=""
+    which will return array of single charcaters in it
+    that is unique
+    */
+
+    /* facts
+    First- there is  6235 Aya 
+   Second - possible of word with redundant =82,482
+   remove duplicate from array 
+   1-using Set . uniqArray= [... new Set(redundArr)]; result :17,630
+   2- using reduce  result = 17,636
+   3- using 
+   4- busing forloop find result : 17742
+    */
+
+
+    /*
+
+        //method 1 using set super 1000 time fast comparing to other methods it  take 4 while other take ~= 2,000
+   // let t1 = performance.now();
+    // const quranWordArr1 = [...new Set(resultArr)];
+   //  console.log(`generateQuranWordsArray Method #1 Set(): time  =${performance.now() - t1} `);
+   // return quranWordArr1; // End of the function
+
+        // method 2 using  reduce
+        t1 = performance.now();
+        const quranWordArr2 = quranWordArr.reduce((unique, item) => {
+          return unique.includes(item) ? unique : [...unique, item];
+        });
+        console.log(`generateQuranWordsArray: Method #2 Reduce: time  =${performance.now() - t1} `);
+    
+        // method 3 using  filter   
+        t1 = performance.now();
+        const quranWordArr3 = quranWordArr.filter((item, index) => quranWordArr.indexOf(item) != index)
+        console.log(`generateQuranWordsArray: Method #3 filter: time  =${performance.now() - t1} `);
+    
+        //method 4 using find 
+        t1 = performance.now();
+        let quranWordArr4 = [""];
+        for (let item of quranWordArr) {
+          if (!quranWordArr4.find(d => d === item)) {
+            quranWordArr4.push(item);
+          }
+        }
+        console.log(`generateQuranWordsArray:Method #4 loop & find: time  =${performance.now() - t1} `);
+    
+        console.log('generateQuranWordsArray:quranWordArr.length =', quranWordArr.length);
+        console.log('generateQuranWordsArray:quranWordArr1.length =', quranWordArr1.length);
+        console.log('generateQuranWordsArray:quranWordArr2.length =', quranWordArr2.length);
+        console.log('generateQuranWordsArray:quranWordArr3.length =', quranWordArr3.length);
+        console.log('generateQuranWordsArray:quranWordArr4.length =', quranWordArr4.length);        
+    */
